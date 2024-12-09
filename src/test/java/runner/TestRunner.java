5829c84f63cc19b0d8d7e9d8c790a0e096ed5773
UmaMaheswarich <test123uma@gmail.com>
2024-12-09 12:41:53 +0530

    new changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/Features"
+		features = "src\\test\\resources\\Features"
 		
 		,glue = {"stepDefinitions"}
 		,dryRun = false
