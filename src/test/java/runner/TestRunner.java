34c550b3eb93e1f95d05ea4d5c2f2d460ca4f2e9
UmaMaheswarich <test123uma@gmail.com>
2024-12-09 12:12:43 +0530

    Testrunner file updated

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,8 +7,8 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/Features/login.feature"
-		//features = "D:\\Javaworks\\BDTCucumber\\src\\test\\resources\\Features\\course.feature"
+		features = "src/test/resources/Features"
+		
 		,glue = {"stepDefinitions"}
 		,dryRun = false
 		,plugin = {"pretty" ,"html:target/HTMLReports/reports.html",
